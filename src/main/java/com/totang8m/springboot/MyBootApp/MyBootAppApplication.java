f9ae8a23569e1e4b2399f9142d1b8aa46a29d306
matozaki-toshinori <matozakitoshinori@team-lab.com>
2020-04-11 14:49:14 +0900

    first commit

---
+++ src/main/java/com/totang8m/springboot/MyBootApp/MyBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.totang8m.springboot.MyBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/totang8m/springboot/MyBootApp/MyBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totang8m.springboot.MyBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
